7ca7e888ed75c34490e73a0d9e0fbc09e01f023a
Jamie Jones <29973406+jjjonesjr33@users.noreply.github.com>
2024-10-05 09:25:38 -0500

    Update __init__.py
    DockstreamSmartRFIDFountain

--- custom_components/petlibro/devices/__init__.py
+++ custom_components/petlibro/devices/__init__.py
@@ -2,9 +2,10 @@ from typing import Dict, Type
 from .device import Device
 from .feeders.granary_feeder import GranaryFeeder
 from .feeders.one_rfid_smart_feeder import OneRFIDSmartFeeder
-
+from .fountain.dockstream_smart_rfid_fountain import DockstreamSmartRFIDFountain  # Import the new class
 
 product_name_map : Dict[str, Type[Device]] = {
     "Granary Feeder": GranaryFeeder,
-    "One RFID Smart Feeder": OneRFIDSmartFeeder
+    "One RFID Smart Feeder": OneRFIDSmartFeeder,
+    "Dockstream Smart RFID Fountain": DockstreamSmartRFIDFountain  # Add the new product
 }
