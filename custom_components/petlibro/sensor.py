17164fb0be663611342bb127545c1b27fd96462c
Jamie Jones <29973406+jjjonesjr33@users.noreply.github.com>
2024-10-04 20:59:31 -0500

    Updated sensor.py
    Added more API data

--- custom_components/petlibro/sensor.py
+++ custom_components/petlibro/sensor.py
@@ -119,43 +119,212 @@ DEVICE_SENSOR_MAP: dict[type[Device], list[PetLibroSensorEntityDescription]] = {
     ],
     OneRFIDSmartFeeder: [
         PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="device_sn",
+            translation_key="device_sn",
+            icon="mdi:identifier",
+            unique_id_fn=lambda device: f"{device.id}-device_sn"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="mac_address",
+            translation_key="mac_address",
+            icon="mdi:network",
+            unique_id_fn=lambda device: f"{device.id}-mac_address"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="wifi_ssid",
+            translation_key="wifi_ssid",
+            icon="mdi:wifi",
+            unique_id_fn=lambda device: f"{device.id}-wifi_ssid"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="wifi_rssi",
+            translation_key="wifi_rssi",
+            icon="mdi:wifi",
+            native_unit_of_measurement="dBm",
+            unique_id_fn=lambda device: f"{device.id}-wifi_rssi"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
             key="remaining_desiccant",
             translation_key="remaining_desiccant",
-            icon="mdi:package"
+            icon="mdi:package",
+            unique_id_fn=lambda device: f"{device.id}-remaining_desiccant"
         ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="barn_door_state",
+            translation_key="barn_door_state",
+            icon="mdi:door",
+            device_class_fn=lambda _: SensorDeviceClass.BINARY,
+            unique_id_fn=lambda device: f"{device.id}-barn_door_state"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="power_mode",
+            translation_key="power_mode",
+            icon="mdi:power-plug",
+            unique_id_fn=lambda device: f"{device.id}-power_mode"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="enable_auto_upgrade",
+            translation_key="enable_auto_upgrade",
+            icon="mdi:refresh-auto",
+            device_class_fn=lambda _: SensorDeviceClass.BINARY,
+            unique_id_fn=lambda device: f"{device.id}-enable_auto_upgrade"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="enable_offline_notice",
+            translation_key="enable_offline_notice",
+            icon="mdi:cloud-off-outline",
+            device_class_fn=lambda _: SensorDeviceClass.BINARY,
+            unique_id_fn=lambda device: f"{device.id}-enable_offline_notice"
+        ),
+    # Controls
+        PetLibroSwitchEntityDescription[OneRFIDSmartFeeder](
+            key="enable_feeding_plan",
+            translation_key="enable_feeding_plan",
+            icon="mdi:calendar-check",
+            unique_id_fn=lambda device: f"{device.id}-enable_feeding_plan"
+        ),
+        PetLibroSwitchEntityDescription[OneRFIDSmartFeeder](
+            key="child_lock_switch",
+            translation_key="child_lock_switch",
+            icon="mdi:lock",
+            unique_id_fn=lambda device: f"{device.id}-child_lock_switch"
+        ),
+    # Switches for Light and Sound
+        PetLibroSwitchEntityDescription[OneRFIDSmartFeeder](
+            key="enable_light",
+            translation_key="enable_light",
+            icon="mdi:lightbulb-on-outline",
+            unique_id_fn=lambda device: f"{device.id}-enable_light"
+        ),
+        PetLibroSwitchEntityDescription[OneRFIDSmartFeeder](
+            key="light_switch",
+            translation_key="light_switch",
+            icon="mdi:lightbulb",
+            unique_id_fn=lambda device: f"{device.id}-light_switch"
+        ),
+        PetLibroSwitchEntityDescription[OneRFIDSmartFeeder](
+            key="enable_sound",
+            translation_key="enable_sound",
+            icon="mdi:volume-high",
+            unique_id_fn=lambda device: f"{device.id}-enable_sound"
+        ),
+        PetLibroSwitchEntityDescription[OneRFIDSmartFeeder](
+            key="sound_switch",
+            translation_key="sound_switch",
+            icon="mdi:volume-mute",
+            unique_id_fn=lambda device: f"{device.id}-sound_switch"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="volume",
+            translation_key="volume",
+            icon="mdi:volume-high",
+            native_unit_of_measurement="%",
+            unique_id_fn=lambda device: f"{device.id}-volume"
+        ),
+    # Sensors for monitoring
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="surplus_grain",
+            translation_key="surplus_grain",
+            icon="mdi:grain",
+            device_class_fn=lambda _: SensorDeviceClass.BINARY,
+            unique_id_fn=lambda device: f"{device.id}-surplus_grain"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="grain_outlet_state",
+            translation_key="grain_outlet_state",
+            icon="mdi:grain",
+            device_class_fn=lambda _: SensorDeviceClass.BINARY,
+            unique_id_fn=lambda device: f"{device.id}-grain_outlet_state"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="battery_state",
+            translation_key="battery_state",
+            icon="mdi:battery",
+            unique_id_fn=lambda device: f"{device.id}-battery_state"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="electric_quantity",
+            translation_key="electric_quantity",
+            icon="mdi:battery",
+            native_unit_of_measurement="%",
+            unique_id_fn=lambda device: f"{device.id}-electric_quantity"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="online",
+            translation_key="online",
+            icon="mdi:wifi",
+            device_class_fn=lambda _: SensorDeviceClass.BINARY,
+            unique_id_fn=lambda device: f"{device.id}-online"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="running_state",
+            translation_key="running_state",
+            icon="mdi:run",
+            unique_id_fn=lambda device: f"{device.id}-running_state"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="whether_in_sleep_mode",
+            translation_key="whether_in_sleep_mode",
+            icon="mdi:sleep",
+            device_class_fn=lambda _: SensorDeviceClass.BINARY,
+            unique_id_fn=lambda device: f"{device.id}-whether_in_sleep_mode"
+        ),
+    # Binary Sensors for Notices
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="enable_low_battery_notice",
+            translation_key="enable_low_battery_notice",
+            icon="mdi:battery-alert",
+            device_class_fn=lambda _: SensorDeviceClass.BINARY,
+            unique_id_fn=lambda device: f"{device.id}-enable_low_battery_notice"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="enable_power_change_notice",
+            translation_key="enable_power_change_notice",
+            icon="mdi:power-plug-alert",
+            device_class_fn=lambda _: SensorDeviceClass.BINARY,
+            unique_id_fn=lambda device: f"{device.id}-enable_power_change_notice"
+        ),
+        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
+            key="enable_grain_outlet_blocked_notice",
+            translation_key="enable_grain_outlet_blocked_notice",
+            icon="mdi:grain-off",
+            device_class_fn=lambda _: SensorDeviceClass.BINARY,
+            unique_id_fn=lambda device: f"{device.id}-enable_grain_outlet_blocked_notice"
+        ),
+    # Feeding
         PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
             key="today_feeding_quantity",
             translation_key="today_feeding_quantity",
             icon="mdi:scale",
             native_unit_of_measurement_fn=unit_of_measurement_feeder,
             device_class_fn=device_class_feeder,
-            state_class=SensorStateClass.TOTAL_INCREASING
+            state_class=SensorStateClass.TOTAL_INCREASING,
+            unique_id_fn=lambda device: f"{device.id}-today_feeding_quantity"
         ),
         PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
             key="today_feeding_times",
             translation_key="today_feeding_times",
             icon="mdi:history",
-            state_class=SensorStateClass.TOTAL_INCREASING
+            state_class=SensorStateClass.TOTAL_INCREASING,
+            unique_id_fn=lambda device: f"{device.id}-today_feeding_times"
         ),
         PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
             key="today_eating_times",
             translation_key="today_eating_times",
             icon="mdi:history",
-            state_class=SensorStateClass.TOTAL_INCREASING
+            state_class=SensorStateClass.TOTAL_INCREASING,
+            unique_id_fn=lambda device: f"{device.id}-today_eating_times"
         ),
         PetLibroSensorEntityDescription[OneRFIDSmartFeeder](            
             key="today_eating_time",
             translation_key="today_eating_time",
             native_unit_of_measurement="s",
             icon="mdi:history",
-            state_class=SensorStateClass.TOTAL_INCREASING
-        ),
-        PetLibroSensorEntityDescription[OneRFIDSmartFeeder](            
-            key="battery_state",
-            translation_key="battery_state",
-            icon="mdi:battery-alert",
+            state_class=SensorStateClass.TOTAL_INCREASING,
+            unique_id_fn=lambda device: f"{device.id}-today_eating_time"
         ),
     ]
+
 }
 
 
