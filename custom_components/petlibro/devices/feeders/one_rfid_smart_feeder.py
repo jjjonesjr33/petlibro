ce31c8eb7f6f5ade6b4ffa06a94e11f0306c0c2f
Jamie Jones <29973406+jjjonesjr33@users.noreply.github.com>
2024-10-05 12:06:01 -0500

    Update one_rfid_smart_feeder.py
    reverting change

--- custom_components/petlibro/devices/feeders/one_rfid_smart_feeder.py
+++ custom_components/petlibro/devices/feeders/one_rfid_smart_feeder.py
@@ -3,8 +3,6 @@ from logging import getLogger
 
 from aiohttp import ClientSession, ClientError  # Import aiohttp and ClientError
 
-from .feeder import Feeder
-from ..feeder import Feeder
 from ..feeders.granary_feeder import GranaryFeeder  # Correct import from GranaryFeeder
 
 _LOGGER = getLogger(__name__)
