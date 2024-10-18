e983375adf9a23deadb2ede54a5ae2eb391ad010
C4-Dimitri <88452320+C4-Dimitri@users.noreply.github.com>
2024-10-18 21:56:17 +0100

    Now Also Changed for OneRFIDFeeder

--- custom_components/petlibro/sensor.py
+++ custom_components/petlibro/sensor.py
@@ -290,6 +290,8 @@ DEVICE_SENSOR_MAP: dict[type[Device], list[PetLibroSensorEntityDescription]] = {
             translation_key="electric_quantity",
             icon="mdi:battery",
             native_unit_of_measurement="%",
+            device_class=SensorDeviceClass.BATTERY,
+            state_class=SensorStateClass.MEASUREMENT,
             name="Battery / AC %"
         ),
         PetLibroSensorEntityDescription[OneRFIDSmartFeeder](
