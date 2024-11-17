9aa7cba432ce9975f3b5937e8bc087b251ae0458
C4-Dimitri <88452320+C4-Dimitri@users.noreply.github.com>
2024-11-17 18:41:04 +0000

    Better Endpoint to get data, data is already in seconds, no need to convert.

--- custom_components/petlibro/devices/feeders/one_rfid_smart_feeder.py
+++ custom_components/petlibro/devices/feeders/one_rfid_smart_feeder.py
@@ -50,15 +50,7 @@ class OneRFIDSmartFeeder(Device):
 
     @property
     def today_eating_time(self) -> int:
-        eating_time_str = self._data.get("grainStatus", {}).get("eatingTime", "0'0''")
-        if not eating_time_str:
-            return 0
-        try:
-            minutes, seconds = map(int, eating_time_str.replace("''", "").split("'"))
-            total_seconds = minutes * 60 + seconds
-        except ValueError:
-            return 0
-        return total_seconds
+        return self._data.get("grainStatus", {}).get("petEatingTime", 0)
 
     @property
     def feeding_plan_state(self) -> bool:
