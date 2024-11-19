017691c52ba040641065f1903b7afb27844aa695
C4-Dimitri <88452320+C4-Dimitri@users.noreply.github.com>
2024-11-19 21:47:13 +0000

    with missing comma...

--- custom_components/petlibro/number.py
+++ custom_components/petlibro/number.py
@@ -72,7 +72,7 @@ DEVICE_NUMBER_MAP: dict[type[Device], list[PetLibroNumberEntityDescription]] = {
             native_min_value=1,
             native_step=1,
             value=lambda device: device.sound_level,
-            method=lambda device: device.set_sound_level
+            method=lambda device: device.set_sound_level,
             name="Sound Level"
         ),
     ]
