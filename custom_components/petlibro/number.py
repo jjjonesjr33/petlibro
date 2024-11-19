aee3f0b1441b0e98b6252aba9780195282dcbd02
C4-Dimitri <88452320+C4-Dimitri@users.noreply.github.com>
2024-11-19 22:22:27 +0000

    dataclasses with no defaults at top instead.

--- custom_components/petlibro/number.py
+++ custom_components/petlibro/number.py
@@ -41,9 +41,9 @@ from .entity import PetLibroEntity, _DeviceT, PetLibroEntityDescription
 class PetLibroNumberEntityDescription(NumberEntityDescription, PetLibroEntityDescription[_DeviceT]):
     """A class that describes device number entities."""
 
-    device_class_fn: Callable[[_DeviceT], NumberDeviceClass | None] = lambda _: None
     value: Callable[[_DeviceT, int], float | None]
     method: Callable[[_DeviceT, int, float], Any]
+    device_class_fn: Callable[[_DeviceT], NumberDeviceClass | None] = lambda _: None
     device_class: Optional[NumberDeviceClass] = None
 
 class PetLibroNumberEntity(PetLibroEntity[_DeviceT], NumberEntity):
