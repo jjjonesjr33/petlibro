b9f9d93feba5a5c70792c12d1ea02da3dabe60d5
zendil <peterson.ted.f@gmail.com>
2026-02-08 00:49:50 -0600

    modify api call to reset desiccant to use object returned from session.post()

--- custom_components/petlibro/api.py
+++ custom_components/petlibro/api.py
@@ -1134,16 +1134,17 @@ class PetLibroAPI:
             if isinstance(response, int):
                 _LOGGER.debug(f"Desiccant reset set successfully, returned code: {response}")
                 return response
+            
+            # Check if response code indicates success
+            if resonse.status == 200:
+                _LOGGER.debug(f"Desiccant reset set successfully, returned code: {response.status}")
+                return response
 
-            # If response is a dictionary (JSON), handle it
-            response_data = await response.json()
-            _LOGGER.debug(f"Desiccant reset response data: {response_data}")
+            # If not response 200 assume failure
+            else:
+                raise PetLibroAPIError(f"Failed to trigger desiccant reset: {response.msg}")
 
-            # Check if the response indicates success
-            if response.status != 200 or response_data.get("code") != 0:
-                raise PetLibroAPIError(f"Failed to trigger desiccant reset: {response_data.get('msg')}")
-
-            return response_data
+            return response
 
         except aiohttp.ClientError as err:
             _LOGGER.error(f"Failed to trigger desiccant reset for device {serial}: {err}")
