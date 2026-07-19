998c7f04934dc2cdfa1c2688556a027795781814
Menelao147 <59983324+Menelao147@users.noreply.github.com>
2026-07-19 22:33:20 +0200

    apply no-pets-debug-patch

--- custom_components/petlibro/select.py
+++ custom_components/petlibro/select.py
@@ -473,7 +473,7 @@ async def async_setup_entry(
 
     # Ensure that the pets are loaded
     if not (pets := hub.pets):
-        _LOGGER.warning("No pets found in hub during select setup.")
+        _LOGGER.debug("No pets found in hub during select setup.")
 
     if not (devices or pets):
         return
