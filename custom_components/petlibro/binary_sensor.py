2c6f66d5be748d838332ec9ffcda7705f0d4d67d
dennismdejong <dennis@caberdo.nl>
2026-07-21 21:16:46 +0200

    feat(#84): add power_connected binary sensor for AirSmartFeeder

--- custom_components/petlibro/binary_sensor.py
+++ custom_components/petlibro/binary_sensor.py
@@ -230,6 +230,14 @@ DEVICE_BINARY_SENSOR_MAP: dict[type[Device], list[PetLibroBinarySensorEntityDesc
             value_fn=lambda device: device.feeding_plan_state,
             name="Feeding Schedule"
         ),
+        PetLibroBinarySensorEntityDescription[AirSmartFeeder](
+            key="power_connected",
+            translation_key="power_connected",
+            icon="mdi:power-plug",
+            device_class=BinarySensorDeviceClass.PLUG,
+            should_report=lambda device: device.power_connected is not None,
+            name="Power Connected"
+        ),
     ],
     GranarySmartFeeder: [
         PetLibroBinarySensorEntityDescription[GranarySmartFeeder](
